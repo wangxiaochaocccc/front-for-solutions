9017357161b446a63ea5934586d9f09ca8a2eead
wangchao <wangxiaochaocccc@163.com>
2022-06-16 11:59:06 +0800

    feat:tailwindcss初始化

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img class="bg-red-900" alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1 class="text-3xl font-bold underline">123445</h1>
 </template>
 
 <style>
