67611c524ec559dba75798c3d9b27b09a8bbb024
wangchao <wangxiaochaocccc@163.com>
2022-06-20 17:16:21 +0800

    feat:使用vueuse获取屏幕响应式宽度

--- src/utils/flexble.js
+++ src/utils/flexble.js
@@ -1,7 +1,11 @@
 import { computed } from 'vue'
 import { PC_DEVICE_WIDTH } from '../constant'
+import { useWindowSize } from '@vueuse/core'
+
+// 获取响应式宽度
+const { width } = useWindowSize()
 
 // 判断是否是移动端设备，依据是看屏幕宽度是否小于指定值
 export const isMobileTerinal = computed(() => {
-  return document.documentElement.clientWidth < PC_DEVICE_WIDTH
+  return width.value < PC_DEVICE_WIDTH
 })
