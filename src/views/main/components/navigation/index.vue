133a0cd5e5daa5d798ba8e18997512c0cd86499f
wangchao <wangxiaochaocccc@163.com>
2022-06-21 11:35:57 +0800

    feat:移动端navigation

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <mobile-navigation v-if="isMobileTerinal" />
+  <mobile-navigation v-if="isMobileTerinal" :data="categoryData" />
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { isMobileTerinal } from '@/utils/flexble'
 import mobileNavigation from './mobile/index.vue'
+import { getCategory } from '@/api/category'
+
+// 数据源
+const categoryData = ref([])
+// 获取分类
+const getCategoryData = async () => {
+  const { categorys } = await getCategory()
+  categoryData.value = categorys
+}
+getCategoryData()
 </script>
 <style lang="scss"></style>
--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="">我是移动端navigation</div>
+  <div class="bg-whit top-0 left-0 z-10 sticky">
+    <ul
+      class="relative flex text-xs text-zinc-600 overflow-x-auto overflow-hidden p-1"
+    >
+      <li
+        v-for="item in data"
+        :key="item.id"
+        class="shrink-0 px-1.5 py-0.5 duration-200"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
-import { getCategory } from '@/api/category'
-
-// 获取分类
-const getCategoryData = async () => {
-  const res = await getCategory()
-  console.log(res)
-}
-getCategoryData()
+// 接收数据
+defineProps({
+  data: {
+    type: Array,
+    required: true
+  }
+})
 </script>
 <style lang="scss"></style>
