c97862a82eee3b03f49d67549eb24a3c8726f2c0
wangchao <wangxiaochaocccc@163.com>
2022-06-22 10:26:45 +0800

    feat:移动slide滑块处理

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,25 +1,38 @@
 <template>
   <div class="bg-whit top-0 left-0 z-10 sticky">
     <ul
+      ref="ulRef"
       class="relative flex text-xs text-zinc-600 overflow-x-auto overflow-hidden p-1"
     >
+      <!-- 滑块 -->
+      <li
+        ref="slideRef"
+        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200 z-[-1]"
+        :style="sliderStyle"
+      ></li>
+      <!-- 面包屑图标 -->
       <li
         class="fixed top-0 right-[-1px] h-4 z-20 px-1 flex items-center bg-white shadow-l-white"
       >
         <svg-icon name="hamburger" class="w-1.5 h-1.5"></svg-icon>
       </li>
       <li
-        v-for="item in data"
+        v-for="(item, index) in data"
         :key="item.id"
         class="shrink-0 px-1.5 py-0.5 duration-200 last:mr-4"
+        :class="{ ' text-zinc-100': index === currentCategoryIndex }"
+        :ref="setItem"
+        @click="handleItemClick(index)"
       >
         {{ item.name }}
       </li>
     </ul>
   </div>
 </template>
-
 <script setup>
+import { onBeforeUpdate, ref, watch } from 'vue'
+import { useScroll } from '@vueuse/core'
+
 // 接收数据
 defineProps({
   data: {
@@ -27,5 +40,41 @@ defineProps({
     required: true
   }
 })
+
+// 滑块
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '60px'
+})
+
+// 选中item的下标
+const currentCategoryIndex = ref(0)
+
+// 获取所有item元素
+let itemsAll = []
+const setItem = (el) => {
+  if (el) {
+    itemsAll.push(el)
+  }
+}
+// 数据变化后，dom更新前处理itemsAll
+onBeforeUpdate(() => {
+  itemsAll = []
+})
+// 获取ul滚动距离
+const ulRef = ref(null)
+const { x: scrollLeft } = useScroll(ulRef)
+// 当currentCategoryIndex变化时 触发效果
+watch(currentCategoryIndex, (val) => {
+  console.log(itemsAll[val])
+  const { left, width } = itemsAll[val].getBoundingClientRect()
+  sliderStyle.value = {
+    transform: `translateX(${scrollLeft.value + left - 10 + 'px'})`,
+    width: width + 'px'
+  }
+})
+// item点击事件
+const handleItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
 </script>
-<style lang="scss"></style>
