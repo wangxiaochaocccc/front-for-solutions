ee30fce9ad9fd1c18c90ad17c91da20cbe922683
wangchao <wangxiaochaocccc@163.com>
2022-08-29 16:58:54 +0800

    feat:infinite组件

---
+++ src/libs/infinite/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <slot />
+    <div class="h-6 py-4" ref="targetRef">
+      <!-- 加载更多 -->
+      <m-svg-icon
+        v-show="isLoading"
+        name="infinite-load"
+        class="w-4 h-4 animate-spin"
+      ></m-svg-icon>
+      <!-- 没有数据了 -->
+      <p class="text-base text-zinc-400 text-center" v-if="isFinished">
+        已经到底了！
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useVModel, useIntersectionObserver } from '@vueuse/core'
+import { ref } from 'vue'
+
+const props = defineEmits({
+  // 是否处于加载状态
+  modelValue: {
+    type: Boolean,
+    require: true
+  },
+  // 是否加载完成
+  isFinished: {
+    type: Boolean,
+    require: true
+  }
+})
+
+const isLoading = useVModel(props)
+const emits = defineEmits(['onload', 'update:ModelValue'])
+// 滚动
+const targetRef = ref(null)
+useIntersectionObserver(targetRef, ([{ isIntersecting }], observerElement) => {
+  // 当加载更多的视图可见时，加载更多数据
+  if (isIntersecting && !loading.value && !props.isFinished) {
+    // 修改加载数据标记
+    loading.value = true
+    // 触发加载更多行为
+    emits('onLoad')
+  }
+})
+</script>
+<style lang="scss"></style>
