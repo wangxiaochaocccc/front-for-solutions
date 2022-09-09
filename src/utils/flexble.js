4ee13d964adc5fe0dac48f72efebff78ec96d6d7
wangchao <wangxiaochaocccc@163.com>
2022-09-09 14:11:54 +0800

    feat:移动端qq登录处理

--- src/utils/flexble.js
+++ src/utils/flexble.js
@@ -7,7 +7,10 @@ const { width } = useWindowSize()
 
 // 判断是否是移动端设备，依据是看屏幕宽度是否小于指定值
 export const isMobileTerinal = computed(() => {
-  return width.value < PC_DEVICE_WIDTH
+  // return width.value < PC_DEVICE_WIDTH
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
 })
 
 // 动态指定rem值 最大为40px,根据屏幕宽度动态计算
--- src/views/login-register/qq-login.vue
+++ src/views/login-register/qq-login.vue
@@ -43,6 +43,8 @@ onMounted(() => {
       }
       // 完成跨页面传输
       brodacast.send(oauthObj)
+      // 针对于 移动端而言：通过移动端触发 QQ 登录会展示三个页面，原页面、QQ 吊起页面、回调页面。并且移动端一个页面展示整屏内容，且无法直接通过 window.close() 关闭，所以在移动端中，我们需要在当前页面继续进行后续操作。
+      oauthLogin(LOGIN_TYPE_QQ, oauthObj)
       // 关闭第三方窗口
       window.close()
     }
