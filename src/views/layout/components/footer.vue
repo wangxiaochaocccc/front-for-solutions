e2e4dfe8ba550fa156213ca191aaa12f0d514e23
wangchao <wangxiaochaocccc@163.com>
2022-09-29 15:46:18 +0800

    feat:添加footer备案号

---
+++ src/views/layout/components/footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class='w-full h-9 flex items-center justify-center text-xs absolute bottom-0 left-0 bg-white shadow-xl'>
+      <a href="https://beian.miit.gov.cn/">京ICP备2022015637号-1</a>
+    </div>
+</template>
+
+<script setup>
+</script>
+<style lang='scss'>
+
+</style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,10 +1,3 @@
-<!--
- * @Author: wangchao
- * @Date: 2022-08-28 22:38:03
- * @LastEditors: wangchao
- * @LastEditTime: 2022-08-31 10:54:28
- * @Description: 
--->
 <template>
   <div class="h-screen">
     <header-vue class="h-header" v-if="!isMobileTerinal"></header-vue>
@@ -12,11 +5,13 @@
       <main-vue></main-vue>
     </div>
     <floating-vue></floating-vue>
+    <footer-vue/>
   </div>
 </template>
 
 <script setup>
 import HeaderVue from './components/header/index.vue'
+import FooterVue from './components/footer.vue'
 import MainVue from './components/main.vue'
 import FloatingVue from './components/floating.vue'
 import { isMobileTerinal } from '@/utils/flexble'
